215c2de8d399d87414799cf79218a79f12ecdc15
aidenlx <31102694+aidenlx@users.noreply.github.com>
2023-07-25 12:12:44 +0800

    fix csv crlf issue

--- deps.ts
+++ deps.ts
@@ -29,9 +29,9 @@ export const envPrefix = "RI_" as const;
 export { exists, ensureDir } from "https://deno.land/std@0.194.0/fs/mod.ts";
 export {
   parse as csvParse,
-  CsvStringifyStream,
   CsvParseStream,
 } from "https://deno.land/std@0.194.0/csv/mod.ts";
+export { CsvStringifyStream } from "https://raw.githubusercontent.com/aidenlx/deno_std/main/csv/mod.ts";
 export { groupBy } from "https://deno.land/std@0.194.0/collections/group_by.ts";
 export { stringify as tomlStringify } from "https://deno.land/std@0.194.0/toml/stringify.ts";
 export { format as fmtBytes } from "https://deno.land/std@0.194.0/fmt/bytes.ts";
