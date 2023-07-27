de2b7ad6cc08ee901d351170a8566bb6a1a44c2b
aidenlx <31102694+aidenlx@users.noreply.github.com>
2023-07-27 10:46:51 +0800

    update hs-stat

--- pipeline/hs-stat.ts
+++ pipeline/hs-stat.ts
@@ -1,20 +1,10 @@
-import {
-  Command,
-  path,
-  ensureDir,
-  EnumType,
-  exists,
-  $,
-  cd,
-  existsSync,
-} from "@/deps.ts";
-import { Res, getBqsrBam, getMarkDupBam, parseIntevals } from "./_res.ts";
+import { Command, EnumType, exists, $ } from "@/deps.ts";
+import { Res, parseIntevals } from "./_res.ts";
 import { nonAscii } from "@/utils/ascii.ts";
 import GATKCollectHsMetrics from "@/pipeline/module/gatk/collectHsMetrics.ts";
 
 export default new Command()
   .name("snv.hs-stat")
-  .option("-o, --out-dir <path:string>", "output directory", { default: "." })
   .type("genomeAssembly", new EnumType(["hs37"]))
   .option("-r, --ref <assembly:genomeAssembly>", "reference genome", {
     required: true,
@@ -27,18 +17,12 @@ export default new Command()
       depends: ["bait-intervals"],
     }
   )
-  .option("-s, --sample <name:string>", "Sample Name", { required: true })
-  .action(async (opts) => {
-    const workPath = path.resolve(opts.outDir),
-      sample = opts.sample;
-    if (nonAscii.test(workPath)) {
+  .arguments("<bam:string>")
+  .action(async (opts, inputBam) => {
+    if (nonAscii.test(inputBam)) {
       throw new Error(
-        "work path should not contain non-ascii characters: " + workPath
-      );
-    }
-    if (nonAscii.test(sample)) {
-      throw new Error(
-        "sample name should not contain non-ascii characters: " + sample
+        "path to input bam file should not contain non-ascii characters: " +
+          inputBam
       );
     }
 
@@ -65,26 +49,6 @@ export default new Command()
     const targetIntervals = target.replace(/\.bed$/, ".interval_list");
     const ref = Res[assembly].refFa;
 
-    await ensureDir(workPath);
-    cd(workPath);
-
-    // const cleanup = genCleanupFunc(opts);
-
-    const bam_dir = "bamfile";
-    const bam_markdup = path.join(bam_dir, getMarkDupBam(sample, assembly));
-    const bam_bqsr = path.join(bam_dir, getBqsrBam(sample, assembly));
-
-    let inputBam;
-    if (existsSync(bam_markdup)) {
-      console.error("Scanning bam file: " + bam_markdup);
-      inputBam = bam_markdup;
-    } else if (existsSync(bam_bqsr)) {
-      console.error("Scanning bam file: " + bam_bqsr);
-      inputBam = bam_bqsr;
-    } else {
-      throw new Error("No bam file found: " + bam_markdup + " or " + bam_bqsr);
-    }
-
     const refDict = ref.replace(/\.fa$/, ".dict");
     if (!(await exists(baitIntervals))) {
       const command = `gatk BedToIntervalList -I ${bait} -O ${baitIntervals} -SD ${refDict}`;
@@ -110,7 +74,7 @@ export default new Command()
     // get two lines after "## METRICS CLASS" and transpose table to list
     await $`rg -A2 '^## METRICS CLASS' ${metrics} \
   | tail -n+2 \
-  | xsv select -d'\\t' 'MEAN_BAIT_COVERAGE,MEAN_TARGET_COVERAGE,46-55' \
+  | xsv select -d'\\t' 'BAIT_SET,MEAN_BAIT_COVERAGE,MEAN_TARGET_COVERAGE,46-55' \
   | awk -F ',' '{ for(i=1; i<=NF; i++) { if (NR==1) { header[i]=$i } else { row[i]=$i } } } END { for(i=1; i<=NF; i++) { print header[i] ": " row[i] } }'\
   > ${report}`;
 
