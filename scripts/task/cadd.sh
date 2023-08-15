43acf00268b5e2f69ec08f2229438745a5db420b
aidenlx <31102694+aidenlx@users.noreply.github.com>
2023-08-15 09:08:44 +0800

    update cadd script

--- scripts/task/cadd.sh
+++ scripts/task/cadd.sh
@@ -12,25 +12,26 @@ conda activate cadd-initial
 PATH=/cluster/home/jiyuan/res/CADD-scripts:$PATH
 
 ASSEMBLY=$(validate_input $1 hg19 hg38)
-SAMPLE_ID=$2
+SRC_VCF=$2
+OUTPUT=$3
+
+[ ! -f "$SRC_VCF" ] && echo "file not found: $SRC_VCF" && exit 1
+
+INPUT_VCF=$(mktemp --suffix .vcf.gz)
+trap "rm -f $INPUT_VCF" EXIT
 
 # only allow variants on autosomes, chrX, chrY
 FILTER='^([1-9]|1[0-9]|2[0-2]|[XY])\t|^#'
 
 if [ $ASSEMBLY == "hg19" ]; then
   ASSEMBLY_CADD="GRCh37"
-  SRC_VCF="$SAMPLE_ID.norm.hs37.vcf.gz"
-  INPUT_VCF="$SAMPLE_ID.norm.hs37.no-chr.vcf.gz"
   # exclude those variants not in CADD
   zcat_safe $SRC_VCF | rg $FILTER | bgzip > $INPUT_VCF
 elif [ $ASSEMBLY == "hg38" ]; then
   ASSEMBLY_CADD="GRCh38"
-  SRC_VCF="$SAMPLE_ID.norm.hg38.vcf.gz"
-  INPUT_VCF="$SAMPLE_ID.norm.hg38.no-chr.vcf.gz"
+  # INPUT_VCF="$SAMPLE_ID.norm.hg38.no-chr.vcf.gz"
   zcat_safe $SRC_VCF | sed 's/^chr//' | rg $FILTER | bgzip > $INPUT_VCF
 fi
-
-OUTPUT=$SAMPLE_ID.cadd.$ASSEMBLY.tsv.gz
 
 CADD.sh -g $ASSEMBLY_CADD -o "$OUTPUT" -c $SLURM_CPUS_PER_TASK $INPUT_VCF
 
