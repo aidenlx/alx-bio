ff37f15aacd7a62d07929374a51bd25e11e2ef99
aidenlx <31102694+aidenlx@users.noreply.github.com>
2023-11-13 21:34:16 +0800

    add canvas

--- pipeline/3GS/canvas.ts
+++ pipeline/3GS/canvas.ts
@@ -3,14 +3,14 @@ import Canvas, { isCanvasSampleType } from "@/pipeline/module/canvas.ts";
 
 export default new Command()
   .name("canvas")
-  .option("--bam <path...>", "bam inputs, format: /a.bam:father", {
+  .option("--bam <paths:string>", "bam inputs, format: /a.bam:father", {
     required: true,
   })
   .option("-o, --out-dir <path>", "output directory", { default: "." })
-  .type("REF", new EnumType(["hg38"]))
-  .option("-r, --ref <ref:REF>", "Ref", { required: true })
+  .type("ASSEMBLY", new EnumType(["hg38"]))
+  .option("-r, --ref <ver:ASSEMBLY>", "Ref", { required: true })
   .action(async (options) => {
-    const inputs = options.bam.map((query) => {
+    const inputs = [options.bam].map((query) => {
       const [bam, type] = query.split(":");
       if (!isCanvasSampleType(type)) {
         throw new Error(`invalid sample type: ${type}`);
